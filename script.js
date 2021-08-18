ffbb48d7ca6eb6ff8e177136bbf5006fed7d10d1
Vitor.Gibim <vitor.gibim@edu.unipar.br>
2021-08-18 15:55:55 -0300

    implement createDaysDez

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+
+
+  function createDaysDez() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const dezDaysListItem = document.querySelector('.days');
+
+    for (let index = 0; index < dezDaysList.length; index++){
+      let day = dezDaysList[index];
+      let dayDezListItem = document.createElement('li');
+      dayDezListItem.innerHTML = day;
+
+      dezDaysListItem.appendChild(dayDezListItem);      
+    }
+  }
+
+  createDaysDez();
